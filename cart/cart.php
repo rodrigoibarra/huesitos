b3e122ffe09d1d612868060259e2fb255b899379
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-08-06 23:17:46 -0500

    responsive de el carrito de compra casi listo

--- cart/cart.php
+++ cart/cart.php
@@ -19,11 +19,12 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 <?php do_action( 'woocommerce_before_cart_table' ); ?>
 
+
 <table class="carrito" cellspacing="0">
 	<thead>
 		<tr>
 			<th class="product-remove">&nbsp;</th>
-			<th class="product-thumbnail">&nbsp;</th>
+			<th class="product-thumbnail">&nbsp;</th> 
 			<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
 			<th class="product-price"><?php _e( 'Price', 'woocommerce' ); ?></th>
 			<th class="product-quantity"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
@@ -47,7 +48,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 							echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf( '<a href="%s" class="remove" title="%s">&times;</a>', esc_url( WC()->cart->get_remove_url( $cart_item_key ) ), __( 'Remove this item', 'woocommerce' ) ), $cart_item_key );
 						?>
 					</td>
-
+				
 					<td class="product-thumbnail">
 						<?php
 							$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
@@ -58,7 +59,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 								printf( '<a href="%s">%s</a>', $_product->get_permalink(), $thumbnail );
 						?>
 					</td>
-
+				
 					<td class="product-name">
 						<?php
 							if ( ! $_product->is_visible() )
@@ -111,7 +112,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 		do_action( 'woocommerce_cart_contents' );
 		?>
 		<tr>
-			<td colspan="6" class="actions">
+			<td colspan="4" class="actions">
 
 				<?php if ( WC()->cart->coupons_enabled() ) { ?>
 					<div class="coupon">
@@ -121,14 +122,18 @@ do_action( 'woocommerce_before_cart' ); ?>
 						<?php do_action('woocommerce_cart_coupon'); ?>
 
 					</div>
+				</td>
 				<?php } ?>
-
-				<input type="submit" class="boton" name="update_cart" value="<?php _e( 'Update Cart', 'woocommerce' ); ?>" /> <input type="submit" class="checkout-boton boton alt wc-forward" name="proceed" value="<?php _e( 'Proceed to Checkout', 'woocommerce' ); ?>" />
-
+				<td>
+				<input type="submit" class="boton" name="update_cart" value="<?php _e( 'Update Cart', 'woocommerce' ); ?>" /> 				</td>
+				<td>
+				<input type="submit" class="checkout-boton boton alt wc-forward" name="proceed" value="<?php _e( 'Proceed to Checkout', 'woocommerce' ); ?>" />
+				</td>
+				<td>
 				<?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
 
 				<?php wp_nonce_field( 'woocommerce-cart' ); ?>
-			</td>
+				</td>
 		</tr>
 
 		<?php do_action( 'woocommerce_after_cart_contents' ); ?>
